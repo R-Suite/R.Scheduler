d737e605a53f36bd5e59bc37b62c06d30c4e570d
jpachansky <jpachansky@gmail.com>
2014-04-29 09:50:41 +0100

    Enable the Scheduler hosted as Windows Service or Console App

--- src/R.Scheduler.Host/Program.cs
+++ src/R.Scheduler.Host/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceProcess;
 using Quartz;
 
 namespace R.Scheduler.Host
 {
     class Program
     {
+        #region Nested classes to support running as service
+
+        public const string ServiceName = "R.Scheduler.Host";
+
+        public class Service : ServiceBase
+        {
+            public Service()
+            {
+                ServiceName = ServiceName;
+            }
+
+            protected override void OnStart(string[] args)
+            {
+                Program.Start(args);
+            }
+
+            protected override void OnStop()
+            {
+                Program.Stop();
+            }
+        }
+
+        #endregion
+
         static void Main(string[] args)
+        {
+            if (!Environment.UserInteractive)
+                // running as service
+                using (var service = new Service())
+                    ServiceBase.Run(service);
+            else
+            {
+                // running as console app
+                Start(args);
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to stop...");
+                Console.ReadKey(true);
+
+                Stop();
+            }
+        }
+
+        private static void Start(string[] args)
         {
             // Initialise scheduler
             Scheduler.Initialize(config =>
@@ -20,6 +60,13 @@ namespace R.Scheduler.Host
 
             IScheduler sched = Scheduler.Instance();
             sched.Start();
+
+        }
+
+        private static void Stop()
+        {
+            IScheduler sched = Scheduler.Instance();
+            sched.Shutdown();
         }
     }
 }

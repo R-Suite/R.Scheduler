5786f7c9cc9fdcef6aca7935db6182ae1dc556e6
Jakub Pachansky <jpachansky@gmail.com>
2014-08-16 23:09:00 +0100

    Update with the latest version of R.MessageBus

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -7,9 +7,11 @@ SET OUTDIR=C:\GIT\RSuite\R.Scheduler\src\
 del *.nupkg
 
 NuGet pack "%OUTDIR%R.Scheduler\R.Scheduler.nuspec"
+NuGet pack "%OUTDIR%R.Scheduler.Contracts\R.Scheduler.Contracts.nuspec"
 
 
-nuget.exe push R.Scheduler.0.0.0.10.nupkg
+nuget.exe push R.Scheduler.0.0.0.13.nupkg
+nuget.exe push R.Scheduler.Contracts.0.0.0.4.nupkg
            
 @ECHO === === === === === === === ===
 

5d95c905f1f61b28a4fbae9435a7ef627e1b3a7c
jpachansky <jpachansky@gmail.com>
2014-12-03 17:15:33 +0000

    Update db types in Postgres table script

--- database/rscheduler/tables_postgres.sql
+++ database/rscheduler/tables_postgres.sql
@@ -5,18 +5,19 @@ CREATE TABLE rsched_audit_history
 (
   time_stamp date,
   action character varying(200) NOT NULL,
-  fire_instance_id character varying(200),
+  fire_instance_id character varying(200) NOT NULL,
   job_name character varying(200),
   job_group character varying(200),
   job_type character varying(200),
   trigger_name character varying(150),
   trigger_group character varying(150),
-  fire_time_utc date,
-  scheduled_fire_time_utc date,
+  fire_time_utc timestamp with time zone,
+  scheduled_fire_time_utc timestamp with time zone,
   job_run_time bigint,
   params text,
   refire_count integer,
   recovering boolean,
   result text,
-  execution_exception text
+  execution_exception text,
+  CONSTRAINT rsched_audit_history_pkey PRIMARY KEY (action, fire_instance_id)
 )

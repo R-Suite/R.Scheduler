669908a23a4eba81a72c517d0e0d4e1348054a34
Jakub Pachansky <jpachansky@gmail.com>
2014-12-03 00:47:55 +0000

    Implement PostgreStore

--- database/rscheduler/tables_postgres.sql
+++ database/rscheduler/tables_postgres.sql
@@ -1,12 +1,22 @@
-DROP TABLE IF EXISTS rsched_custom_jobs;
+DROP TABLE IF EXISTS rsched_audit_history;
 
 
-CREATE TABLE rsched_custom_jobs
+CREATE TABLE rsched_audit_history
 (
-  id uuid NOT NULL,
-  name character varying(200) NOT NULL,
+  time_stamp date,
+  action character varying(200) NOT NULL,
+  fire_instance_id character varying(200),
+  job_name character varying(200),
+  job_group character varying(200),
+  job_type character varying(200),
+  trigger_name character varying(150),
+  trigger_group character varying(150),
+  fire_time_utc date,
+  scheduled_fire_time_utc date,
+  job_run_time bigint,
   params text,
-  job_type character varying(200) NOT NULL,
-  CONSTRAINT rsched_custom_jobs_pkey PRIMARY KEY (id),
-  CONSTRAINT rsched_custom_jobs_name_job_type_key UNIQUE (name, job_type)
+  refire_count integer,
+  recovering boolean,
+  result text,
+  execution_exception text
 )

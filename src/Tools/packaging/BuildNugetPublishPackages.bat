2a98594d24a0cd8708b6f344fc36532f7b8be1f7
Jakub Pachansky <jPachansky@Ruffer.co.uk>
2019-05-28 17:14:29 +0100

    Increase package version

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -7,11 +7,11 @@ SET OUTDIR=C:\Git\R.Scheduler\src\
 del *.nupkg
 
 NuGet pack "%OUTDIR%R.Scheduler\R.Scheduler.nuspec"
-NuGet pack "%OUTDIR%R.Scheduler.Contracts\R.Scheduler.Contracts.nuspec"
+::NuGet pack "%OUTDIR%R.Scheduler.Contracts\R.Scheduler.Contracts.nuspec"
 
 
-nuget.exe push R.Scheduler.1.2.18.nupkg -Source https://www.nuget.org/api/v2/package
-nuget.exe push R.Scheduler.Contracts.1.2.4.nupkg -Source https://www.nuget.org/api/v2/package
+nuget.exe push R.Scheduler.1.2.19.nupkg -Source https://www.nuget.org/api/v2/package
+::nuget.exe push R.Scheduler.Contracts.1.2.4.nupkg -Source https://www.nuget.org/api/v2/package
 
            
 @ECHO === === === === === === === ===
